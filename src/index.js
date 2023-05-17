409228b4df1a3a7421a24c27cf75e95428661f58
mutugi-c <mutugichris@gmail.com>
2023-05-17 19:27:33 +0200

    Fetch game scores from API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import _ from 'lodash';
 import './styles.css';
 
-function component() {
-  const element = document.createElement('div');
+const refreshBtn = document.getElementById('refresh-btn');
+const scoresTable = document.getElementById('scores-table');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['', ''], ' ');
+refreshBtn.addEventListener('click', async () => {
+  try {
+    const response = await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WUuOnoz8VP2uepBIRJdR/scores/'
+    );
+    const data = await response.json();
 
-  return element;
-}
+    scoresTable.innerHTML = '';
 
-document.body.appendChild(component());
+    data.result.forEach((score) => {
+      const newScore = document.createElement('tr');
+      newScore.innerHTML = `
+    <td class='col-4'>${score.user}</td>
+    <td class='col-6'>${score.score}</td>
+  `;
+      scoresTable.appendChild(newScore);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
