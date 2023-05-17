36d353528c96c878a7fbd3c717306d578efd78c1
mutugi-c <mutugichris@gmail.com>
2023-05-17 19:28:30 +0200

    Send game scores to API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 
 const refreshBtn = document.getElementById('refresh-btn');
+const addScoreForm = document.getElementById('add-score-form');
 const scoresTable = document.getElementById('scores-table');
 
 refreshBtn.addEventListener('click', async () => {
@@ -25,3 +26,34 @@ refreshBtn.addEventListener('click', async () => {
   }
 });
 
+addScoreForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const userInput = document.getElementById('user-input').value;
+  const scoreInput = document.getElementById('score-input').value;
+
+  if (!userInput || !scoreInput) {
+    return;
+  }
+
+  const data = {
+    user: userInput,
+    score: parseInt(scoreInput),
+  };
+
+  try {
+    await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WUuOnoz8VP2uepBIRJdR/scores/',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    );
+    addScoreForm.reset();
+  } catch (error) {
+    console.error(error);
+  }
+});
